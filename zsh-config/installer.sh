deda8d4d2217728dd4939600d238e6802511818f
AlphaX3Delta <40696289+MaximilianBetz@users.noreply.github.com>
2019-05-27 17:29:13 +0200

    Update installer.sh

--- zsh-config/installer.sh
+++ zsh-config/installer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update
+git clone https://github.com/zsh-users/antigen.git
 sudo apt-get install neofetch
 sudo apt-get install make
 sudo apt-get install cmake
 sudo apt-get install python
-cp -r ~/.antigen/ /home/
-cp ~/.bashrc/ /home/
-cp ~/.zshrc/ /home/
+cp -r ~/antigen/ /home/
+cp ~/bashrc/ /home/
+cp ~/zshrc/ /home/
 
 message=ready
 echo "$message"
